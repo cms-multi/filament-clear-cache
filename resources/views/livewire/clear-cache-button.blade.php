c57a9d03bd23ce9c5312548c9fd45eb89f7a26ed
Ben Johnson <howdu@users.noreply.github.com>
2023-01-02 13:29:55 +0000

    Button loading state. Force rounded

--- resources/views/livewire/clear-cache-button.blade.php
+++ resources/views/livewire/clear-cache-button.blade.php
@@ -1,6 +1,7 @@
-<x-filament::button wire:click="clear" type="button" style="margin-inline-start: 1rem;" @class([
+<x-filament::button wire:click="clear" type="button" color="secondary" style="margin-inline-start: 1rem;border-radius:100%;border:none" @class([
         'flex flex-shrink-0 w-10 h-10 rounded-full bg-gray-200 items-center justify-center',
         'dark:bg-gray-900' => config('filament.dark_mode'),
     ]) x-tooltip.raw="{{ __('filament-clear-cache::general.clear_cache') }}">
-    <x-heroicon-s-trash class="w-4 h-4"/>
+    <x-heroicon-s-trash wire:loading.remove.delay class="w-5 h-5"/>
+    <x-filament-support::loading-indicator x-cloak wire:loading.delay wire:target="clear" class="filament-button-icon w-5 h-5"/>
 </x-filament::button>
